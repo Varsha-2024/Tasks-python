428040b68f769c1f47ef94ed2230f8d873424b55
Veldanda Varsha <64470468+Varsha-2024@users.noreply.github.com>
2023-11-15 12:54:54 +0530

    Add files via upload

---
+++ pythontask_4.py
@@ -0,0 +1,59 @@
+#Arthemetic Operators
+a = 7
+b = 2
+
+# addition
+print ('Sum: ', a + b)  
+
+# subtraction
+print ('Subtraction: ', a - b)   
+
+# multiplication
+print ('Multiplication: ', a * b)  
+
+# division
+print ('Division: ', a / b) 
+
+# floor division
+print ('Floor Division: ', a // b)
+
+# modulo
+print ('Modulo: ', a % b)  
+
+# a to the power b
+print ('Power: ', a ** b)
+
+#Assignment Operators
+a = 10
+b = 5
+a += b      # a = a + b
+print(a)
+a -= b
+print(a)
+a *= b
+print(a)
+a /= b
+print(a)
+
+#Comparission Operators
+a = 5
+b = 2
+print('a == b =', a == b)
+print('a != b =', a != b)
+print('a > b =', a > b)
+print('a < b =', a < b)
+print('a >= b =', a >= b)
+print('a <= b =', a <= b)
+
+#Logical Operators
+a = 5
+b = 6
+print((a > 2) and (b >= 6))    # T and T =T
+print((a > 2) or (b >= 6))    # T or T = T
+print(not((a > 2) or (b >= 6))) #not(T) = F
+
+#Membership Operator
+x = 'Hello world'
+print('H' in x)  #  True
+print('hello' not in x)  #  True
+
